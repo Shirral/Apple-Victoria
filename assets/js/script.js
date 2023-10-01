a648fa2117b48e4a998d0262102953d5dfb721c1
Shirral <132143164+Shirral@users.noreply.github.com>
2023-10-01 22:12:20 +0100

    Add applesAppear function to script.js
    Add the skeleton of the function that will make the apples appear at random spots of the container div.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,32 @@ function pickingMode() {
     $('#screen').html(`
     <button id='test'>Make an apple appear...</button>
     <div id='apple-div'>
-        <img src="assets/images/apple.png" class="apple-picture">
+        
     </div>
     `);
     $('#screen').addClass('picking-screen');
 };
 
 $('button').on('click', pickingMode);
+
+/*-----------*/
+
+function applesAppear() {
+
+    let applePositionTop = Math.floor(Math.random()*100) + '%';
+    let applePositionLeft = Math.floor(Math.random()*100) + '%';
+    let applePicture = '<img src="assets/images/apple.png" class="apple-picture">';
+    $('.apple-picture').css({'top': applePositionTop, 'left': applePositionLeft});
+    $('#apple-div').append(applePicture);
+
+    console.log(applePositionTop, applePositionLeft);
+
+}
+
+function testTest() {
+    
+    console.log('test works!');
+}
+
+/*$(document).on('click', '#test', testTest);*/
+$(document).on('click', '#test', applesAppear);
