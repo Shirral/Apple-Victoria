5557a0a3baf9ae71435aec5e16320edf7e32935a
Shirral <132143164+Shirral@users.noreply.github.com>
2023-10-02 13:26:45 +0100

    Fix a bug in appleType and applesAppear functions
    Fix a bug where the apples would appear at a fixed spot first and only then be moved into their own randomized position. Also tidy up the code a bit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,54 +16,45 @@ function applesAppear() {
 
     let applePositionTop = Math.floor(Math.random()*100) + '%';
     let applePositionLeft = Math.floor(Math.random()*100) + '%';
-    let applePicture = '<img src="assets/images/apple.png" class="apple-picture">';
-    $('.apple-picture').last().css({'top': applePositionTop, 'left': applePositionLeft});
+
+    function appleType() {
+    
+        let applePicturesArray = [`<img src="assets/images/apple.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}"></img>`, `<img src="assets/images/apple2.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}">`, `<img src="assets/images/apple3.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}">`, `<img src="assets/images/apple-bad.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}">`, `<img src="assets/images/apple-bad2.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}">`, `<img src="assets/images/apple-bad3.png" class="apple-picture" style="top:${applePositionTop}; left:${applePositionLeft}">`];
+    
+        let appleNum = Math.floor(Math.random()*100)+1;
+    
+        console.log(appleNum);
+    
+        switch (true) {
+            case (appleNum <= 17):
+                console.log('1');
+                return applePicturesArray[0];
+            case (appleNum <= 34):
+                console.log('2');
+                return applePicturesArray[1];
+            case (appleNum <= 51):
+                console.log('3');
+                return applePicturesArray[2];
+            case (appleNum <= 67):
+                console.log('4');
+                return applePicturesArray[3];
+            case (appleNum <= 83):
+                console.log('5');
+                return applePicturesArray[4];
+            case (appleNum <= 100):
+                console.log('6');
+                return applePicturesArray[5];
+        }
+    
+    }
+
+    console.log(appleType());
+
+    let applePicture = `${appleType()}`;
     $('#apple-div').append(applePicture);
 
     console.log(applePositionTop, applePositionLeft);
 
 }
 
-
-
-
-function appleType() {
-    
-    let applePicturesArray = ['<img src="assets/images/apple.png" class="apple-picture"></img>', '<img src="assets/images/apple2.png" class="apple-picture">', '<img src="assets/images/apple3.png" class="apple-picture">', '<img src="assets/images/apple-bad.png" class="apple-picture">', '<img src="assets/images/apple-bad2.png" class="apple-picture">', '<img src="assets/images/apple-bad3.png" class="apple-picture">'];
-
-    let appleNum = Math.floor(Math.random()*100)+1;
-
-    console.log(appleNum);
-
-    switch (true) {
-        case (appleNum <= 17):
-            console.log('1');
-            return applePicturesArray[0];
-        case (appleNum <= 34):
-            console.log('2');
-            return applePicturesArray[1];
-        case (appleNum <= 51):
-            console.log('3');
-            return applePicturesArray[2];
-        case (appleNum <= 67):
-            console.log('4');
-            return applePicturesArray[3];
-        case (appleNum <= 83):
-            console.log('5');
-            return applePicturesArray[4];
-        case (appleNum <= 100):
-            console.log('6');
-            return applePicturesArray[5];
-    }
-
-}
-
-console.log(appleType());
-
-function testTest() {
-    
-    console.log('test works!');
-}
-
-/*$(document).on('click', '#test', testTest);*/
 $(document).on('click', '#test', applesAppear);
